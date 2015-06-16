6209b5e2af883fa901c74ce6a464d611960b12f0
Eddie Rendleman <henjee1@gmail.com>
2015-06-16 15:36:16 -0400

    I had exits as array parameter, but not going to be all inclusive enough this way

--- samplegame.rb
+++ samplegame.rb
@@ -1,1 +1,16 @@
-puts "You are in a sunlit arboretum. There is a door to the north."
+require_relative("samplegameroomclass")
+require_relative("samplegamegameclass")
+
+
+room1 = Room.new(["N"])
+room2 = Room.new(["E", "W"])
+
+puts "You are in a dark room. There is a door to the north.  Type a direction(N,S,E, or W) to go that direction"
+puts ">"
+direction = gets.chomp
+puts @exits.class
+if @exits.include? direction 
+	puts "You are in the forest. There is a lot of light. There is a bear sleeping."
+else 
+	puts "There is no exit in that direction"
+end
---
+++ samplegameroomclass.rb
@@ -0,0 +1,8 @@
+
+
+class Room 
+	attr_accessor :exits
+	def initialize(exits, bear)
+		@exits = exits
+	end
+end
