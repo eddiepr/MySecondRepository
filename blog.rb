856047d8cbf10d802dd792258700f05cb58db466
Eddie Rendleman <henjee1@gmail.com>
2015-06-16 14:30:27 -0400

    not sure if I commited my final version last night

--- blog.rb
+++ blog.rb
@@ -8,7 +8,7 @@ class Blog
 	end
 	def publish_front_page
 		@posts.each do |i|
-			puts i 
+			i.print_post 
 		end
 	end
 end
@@ -18,7 +18,7 @@ class Post
 		@title = title
 		@date = date
 		@text = text
-		return [@title, @date, @text]
+		#return [@title, @date, @text]
 	end
 	def print_post
 		puts @title
@@ -29,10 +29,13 @@ class Post
 	end
 end
 
-post1 = Post.new("About Me", "June 15, 2015", "I am Eddie.")
-post2 = Post.new("favorite movies", "June 15, 2015", "Titanic, Up")
+post1 = Post.new("About Me", "June 13, 2015", "I am Eddie. I am 29.  I like Whole Foods")
+post2 = Post.new("Favorite Movies", "June 12, 2015", "Titanic, Up")
+post3 = Post.new("Things I Did Today", "June 15, 2015", "I did Ironhack today")
 #each post is an item of the blog array
 blog1 = Blog.new
 blog1.add_post(post1)
 blog1.add_post(post2)
+blog1.add_post(post3)
 blog1.publish_front_page
+post1.print_post
