229e59e0c6e82c373c17dcd50f510ca64af76453
Eddie Rendleman <henjee1@gmail.com>
2015-06-15 20:23:25 -0400

    made some changes

--- blog.rb
+++ blog.rb
@@ -3,19 +3,22 @@ class Blog
 		@posts = []
 	end
 	def add_post(post) 
-		@posts.push(post)
-		
+		@post = post
+		@posts.push(post)	
 	end
 	def publish_front_page
-		puts @posts
+		@posts.each do |i|
+			puts i 
+		end
 	end
 end
 
-class Post
+class Post 
 	def initialize(title, date, text)
 		@title = title
 		@date = date
 		@text = text
+		return [@title, @date, @text]
 	end
 	def print_post
 		puts @title
@@ -30,5 +33,6 @@ post1 = Post.new("About Me", "June 15, 2015", "I am Eddie.")
 post2 = Post.new("favorite movies", "June 15, 2015", "Titanic, Up")
 #each post is an item of the blog array
 blog1 = Blog.new
-blog1.create_front_page(post1, post2)
+blog1.add_post(post1)
+blog1.add_post(post2)
 blog1.publish_front_page
