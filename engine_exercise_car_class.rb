f05db5a588b4244adcdad69d7ed406fe4b7fce87
Eddie Rendleman <henjee1@gmail.com>
2015-06-17 11:45:28 -0400

    updated, so that each new car makes an independent car sound via parameter at creation of new car instance

--- engine_exercise_car_class.rb
+++ engine_exercise_car_class.rb
@@ -3,12 +3,13 @@ require_relative("engine_exercise_engine2_class")
 require_relative("engine_exercise_engine3_class")
 
 class Car
-	def initialize(engine)
+	def initialize(car_sound, engine)
+		@car_sound = car_sound
 		@engine = engine
 	end
 	def start
 		@engine.sound
-		puts "whrrrrr" #other sound that a car makes aside from the engine
+		puts @car_sound#other sound that a car makes aside from the engine
 	end
 end
 
