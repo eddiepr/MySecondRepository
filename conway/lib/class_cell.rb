a21c330993d20b6d893110517389827bc54d47b6
Eddie Rendleman <henjee1@gmail.com>
2015-06-25 17:35:22 -0400

    iteration one complete unless I'm wrong

---
+++ conway/lib/class_cell.rb
@@ -0,0 +1,41 @@
+class Cell
+	def initialize(state, neighbours)
+		@state = state # either 1 or 0 for alive or dead
+		@neighbours = neighbours # an array that represents the surrounding cells
+		@sum = @neighbours.inject(0) do |sum,x| 
+			sum + x 
+		end
+	end
+
+	def regenerate
+		if @state == 0 && @sum == 3 
+			return true
+		else
+			return false
+		end
+	end
+
+	def move_on
+		if @state == 1 && (@sum == 2 || @sum == 3)
+			return true
+		else
+			return false
+		end
+	end
+
+	def under_population
+		if @state == 1 && (@sum < 2)
+			return true
+		else
+			return false
+		end
+	end
+
+	def over_population
+		if @state == 1 && (@sum > 3)
+			return true
+		else
+			return false
+		end
+	end
+end
