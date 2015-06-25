5978cf0172b8d2f2e08995d61a55f93d21f1d538
Eddie Rendleman <henjee1@gmail.com>
2015-06-25 17:55:08 -0400

    updated iteration 1.  I combined the 4 live or die methods into one live_on method

--- conway/lib/class_cell.rb
+++ conway/lib/class_cell.rb
@@ -7,35 +7,49 @@ class Cell
 		end
 	end
 
-	def regenerate
+	def live_on
 		if @state == 0 && @sum == 3 
 			return true
+		elsif @state == 1 && (@sum == 2 || @sum == 3)
+			return true
+		elsif @state == 1 && (@sum < 2)
+			return false
+		elsif @state == 1 && (@sum > 3)
+			return false
 		else
 			return false
 		end
 	end
 
-	def move_on
-		if @state == 1 && (@sum == 2 || @sum == 3)
-			return true
-		else
-			return false
-		end
-	end
+	# def regenerate
+	# 	if @state == 0 && @sum == 3 
+	# 		return true
+	# 	else
+	# 		return false
+	# 	end
+	# end
 
-	def under_population
-		if @state == 1 && (@sum < 2)
-			return true
-		else
-			return false
-		end
-	end
+	# def move_on
+	# 	if @state == 1 && (@sum == 2 || @sum == 3)
+	# 		return true
+	# 	else
+	# 		return false
+	# 	end
+	# end
 
-	def over_population
-		if @state == 1 && (@sum > 3)
-			return true
-		else
-			return false
-		end
-	end
+	# def under_population
+	# 	if @state == 1 && (@sum < 2)
+	# 		return true
+	# 	else
+	# 		return false
+	# 	end
+	# end
+
+	# def over_population
+	# 	if @state == 1 && (@sum > 3)
+	# 		return true
+	# 	else
+	# 		return false
+	# 	end
+	# end
 end
--- conway/spec/conway_spec.rb
+++ conway/spec/conway_spec.rb
@@ -2,92 +2,118 @@ require_relative("../lib/class_cell.rb")
 
 RSpec.describe("Cell") do 
 
-    it ("A live cell with fewer than two live neighbours dies, as if caused by under-population.") do
-      cell1 = Cell.new(1, [0,0,0,0,0,0,0,1])
-      expect(cell1.regenerate).to eq(false)
-    end    
-
-    it ("A live cell with two or three live neighbours lives on to the next generation.") do
-      cell1 = Cell.new(1, [0,0,0,0,0,1,1,1])
-      expect(cell1.regenerate).to eq(false)
-    end
-
-    it ("A live cell with more than three live neighbours dies, as if by overcrowding.") do
-      cell1 = Cell.new(1, [0,0,0,0,1,1,1,1])
-      expect(cell1.regenerate).to eq(false)
-    end
-
-    it ("regenerates a dead cell from a dead cell if it has 0 neighbours") do
-      cell1 = Cell.new(0, [0,0,0,0,0,1,1,1])
-      expect(cell1.regenerate).to eq(true)
-    end
-
-
-
-
-    it ("A live cell with fewer than two live neighbours dies, as if caused by under-population.") do
-      cell1 = Cell.new(1, [0,0,0,0,0,0,0,1])
-      expect(cell1.move_on).to eq(false)
-    end    
-
-    it ("A live cell with two or three live neighbours lives on to the next generation.") do
-      cell1 = Cell.new(1, [0,0,0,0,0,1,1,1])
-      expect(cell1.move_on).to eq(true)
-    end
-
-    it ("A live cell with more than three live neighbours dies, as if by overcrowding.") do
-      cell1 = Cell.new(1, [0,0,0,0,1,1,1,1])
-      expect(cell1.move_on).to eq(false)
-    end
-
-    it ("regenerates a dead cell from a dead cell if it has 0 neighbours") do
-      cell1 = Cell.new(0, [0,0,0,0,0,1,1,1])
-      expect(cell1.move_on).to eq(false)
-    end
-  # write more tests to cover all cases
-
-
 
   it ("A live cell with fewer than two live neighbours dies, as if caused by under-population.") do
       cell1 = Cell.new(1, [0,0,0,0,0,0,0,1])
-      expect(cell1.under_population).to eq(true)
+      expect(cell1.live_on).to eq(false)
     end    
 
     it ("A live cell with two or three live neighbours lives on to the next generation.") do
       cell1 = Cell.new(1, [0,0,0,0,0,1,1,1])
-      expect(cell1.under_population).to eq(false)
+      expect(cell1.live_on).to eq(true)
     end
 
     it ("A live cell with more than three live neighbours dies, as if by overcrowding.") do
       cell1 = Cell.new(1, [0,0,0,0,1,1,1,1])
-      expect(cell1.under_population).to eq(false)
+      expect(cell1.live_on).to eq(false)
     end
 
     it ("regenerates a dead cell from a dead cell if it has 0 neighbours") do
       cell1 = Cell.new(0, [0,0,0,0,0,1,1,1])
-      expect(cell1.under_population).to eq(false)
+      expect(cell1.live_on).to eq(true)
     end
 
 
 
-    it ("A live cell with fewer than two live neighbours dies, as if caused by under-population.") do
-      cell1 = Cell.new(1, [0,0,0,0,0,0,0,1])
-      expect(cell1.over_population).to eq(false)
-    end    
 
-    it ("A live cell with two or three live neighbours lives on to the next generation.") do
-      cell1 = Cell.new(1, [0,0,0,0,0,1,1,1])
-      expect(cell1.over_population).to eq(false)
-    end
 
-    it ("A live cell with more than three live neighbours dies, as if by overcrowding.") do
-      cell1 = Cell.new(1, [0,0,0,0,1,1,1,1])
-      expect(cell1.over_population).to eq(true)
-    end
 
-    it ("regenerates a dead cell from a dead cell if it has 0 neighbours") do
-      cell1 = Cell.new(0, [0,0,0,0,0,1,1,1])
-      expect(cell1.over_population).to eq(false)
-    end
+  #   it ("A live cell with fewer than two live neighbours dies, as if caused by under-population.") do
+  #     cell1 = Cell.new(1, [0,0,0,0,0,0,0,1])
+  #     expect(cell1.regenerate).to eq(false)
+  #   end    
+
+  #   it ("A live cell with two or three live neighbours lives on to the next generation.") do
+  #     cell1 = Cell.new(1, [0,0,0,0,0,1,1,1])
+  #     expect(cell1.regenerate).to eq(false)
+  #   end
+
+  #   it ("A live cell with more than three live neighbours dies, as if by overcrowding.") do
+  #     cell1 = Cell.new(1, [0,0,0,0,1,1,1,1])
+  #     expect(cell1.regenerate).to eq(false)
+  #   end
+
+  #   it ("regenerates a dead cell from a dead cell if it has 0 neighbours") do
+  #     cell1 = Cell.new(0, [0,0,0,0,0,1,1,1])
+  #     expect(cell1.regenerate).to eq(true)
+  #   end
+
+
+
+
+  #   it ("A live cell with fewer than two live neighbours dies, as if caused by under-population.") do
+  #     cell1 = Cell.new(1, [0,0,0,0,0,0,0,1])
+  #     expect(cell1.move_on).to eq(false)
+  #   end    
+
+  #   it ("A live cell with two or three live neighbours lives on to the next generation.") do
+  #     cell1 = Cell.new(1, [0,0,0,0,0,1,1,1])
+  #     expect(cell1.move_on).to eq(true)
+  #   end
+
+  #   it ("A live cell with more than three live neighbours dies, as if by overcrowding.") do
+  #     cell1 = Cell.new(1, [0,0,0,0,1,1,1,1])
+  #     expect(cell1.move_on).to eq(false)
+  #   end
+
+  #   it ("regenerates a dead cell from a dead cell if it has 0 neighbours") do
+  #     cell1 = Cell.new(0, [0,0,0,0,0,1,1,1])
+  #     expect(cell1.move_on).to eq(false)
+  #   end
+  # # write more tests to cover all cases
+
+
+
+  # it ("A live cell with fewer than two live neighbours dies, as if caused by under-population.") do
+  #     cell1 = Cell.new(1, [0,0,0,0,0,0,0,1])
+  #     expect(cell1.under_population).to eq(true)
+  #   end    
+
+  #   it ("A live cell with two or three live neighbours lives on to the next generation.") do
+  #     cell1 = Cell.new(1, [0,0,0,0,0,1,1,1])
+  #     expect(cell1.under_population).to eq(false)
+  #   end
+
+  #   it ("A live cell with more than three live neighbours dies, as if by overcrowding.") do
+  #     cell1 = Cell.new(1, [0,0,0,0,1,1,1,1])
+  #     expect(cell1.under_population).to eq(false)
+  #   end
+
+  #   it ("regenerates a dead cell from a dead cell if it has 0 neighbours") do
+  #     cell1 = Cell.new(0, [0,0,0,0,0,1,1,1])
+  #     expect(cell1.under_population).to eq(false)
+  #   end
+
+
+
+  #   it ("A live cell with fewer than two live neighbours dies, as if caused by under-population.") do
+  #     cell1 = Cell.new(1, [0,0,0,0,0,0,0,1])
+  #     expect(cell1.over_population).to eq(false)
+  #   end    
+
+  #   it ("A live cell with two or three live neighbours lives on to the next generation.") do
+  #     cell1 = Cell.new(1, [0,0,0,0,0,1,1,1])
+  #     expect(cell1.over_population).to eq(false)
+  #   end
+
+  #   it ("A live cell with more than three live neighbours dies, as if by overcrowding.") do
+  #     cell1 = Cell.new(1, [0,0,0,0,1,1,1,1])
+  #     expect(cell1.over_population).to eq(true)
+  #   end
+
+  #   it ("regenerates a dead cell from a dead cell if it has 0 neighbours") do
+  #     cell1 = Cell.new(0, [0,0,0,0,0,1,1,1])
+  #     expect(cell1.over_population).to eq(false)
+  #   end
 
 end
