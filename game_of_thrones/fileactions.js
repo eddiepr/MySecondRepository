35a33c0dd568a43fb8e1106e041dab72eaca09f8
Eddie Rendleman <henjee1@gmail.com>
2015-07-01 19:20:57 -0400

    through iteration 4, but it needs to be restructured with more callbacks

--- game_of_thrones/fileactions.js
+++ game_of_thrones/fileactions.js
@@ -1,49 +1,45 @@
-
-
-// function fileactions(err, file){ 
-//     if (err) {
-//         throw err;
-//     }
-//     var episodes = JSON.parse(file)
-//     display(episodes)
-// }
-
-// function display(episodes){
-// 	console.log(episodes[0])
-// }
-
-
-
-
 function fileActions(err, file){ 
     if (err) {
         console.log(err);
     }
+    console.log("The File has been read")
     var episodes = JSON.parse(file)
+   
+    var sorted_episodes = episodes.sort(function(a, b)
+    {
+    	return (a.episode_number)-(b.episode_number);
+    });
 
-    // var stars = function (episode) {
-    // 	var rate = episode.rating;
-    // 	for (i=1; i < parseInt(rate) + 1; i++)
-    // 	{
-    // 		return "*"
-    // 	}
-    // }
-
-    episodes.forEach(function(episode) {
+    var filtered_sorted_episodes = sorted_episodes.filter(function(episode) 
+    {
+    	return episode.rating >= 8.5;
+    })
+    filtered_sorted_episodes.forEach(function(episode) {
+    	// console.log(numbers);
 	    console.log("Title: " + episode.title + "Episode: #" + episode.episode_number);
 	    console.log(episode.description);
 	    var star_array = [];
+
     	for (i=1; i < parseInt(episode.rating) + 1; i++) 
 		{
-			star_array.push("*")
+			star_array.push("*");
 		}
 		var star_string = star_array.toString()
 		for (i=1; i < parseInt(episode.rating) * 2; i++) 
 		{
 			star_string = star_string.replace(',','');
 		}
-	    console.log("Rating:" + episode.rating + " " + star_string); //+ stars(episode));		
+	    console.log("Rating:" + episode.rating + " " + star_string);
+	    console.log('\n');		
 	});
 }
 
 module.exports = fileActions
+
+
+
+
+
+
+
+
--- game_of_thrones/main.js
+++ game_of_thrones/main.js
@@ -1,3 +1,4 @@
 var fs = require('fs');
 var fileActions = require('./fileactions')
 fs.readFile("./game_of_thrones.json", 'utf8', fileActions);
+
